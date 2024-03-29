a6114006f1a3848d25a45c506c43934f4252f293
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2024-03-29 14:01:11 +0000

    add camera

--- player.gd
+++ player.gd
@@ -1,16 +1,16 @@
 extends CharacterBody3D
 
+# How fast the player moves in meters per second.
 @export var speed = 14
-
+# The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
-# The target_velocity is a 3D vector combining a speed with a direction. 
-# Here, we define it as a property because we want to update and reuse its value across frames.
 var target_velocity = Vector3.ZERO
+
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
-	
+
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -19,3 +19,19 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
